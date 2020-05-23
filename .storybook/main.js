3e728a1a259562731496f231ad0ca807a4ac9afb
kafkae <villas1998@mail.ru>
2020-05-24 03:06:06 +0400

    feat: удалил не нужные коммиты

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.(ts|tsx|js|jsx|mdx)'],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
     '@storybook/addon-links',
     {
       name: "@storybook/addon-docs",
