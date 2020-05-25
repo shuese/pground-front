34808e4cabd4271006d28d74c3fe1a997844df6c
kafkae <villas1998@mail.ru>
2020-05-25 15:39:46 +0400

    feat: добавил Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
+
+Sentry.init({ dsn: 'https://06b2e364cb154a47bd5dd72d5b203e98@o397560.ingest.sentry.io/5252122' });
 
 ReactDOM.render(
   <React.StrictMode>
